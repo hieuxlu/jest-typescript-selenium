eb9491b6213f98f68e7179b4ffc72ba2f506a060
Hieu Do <xlu.untitled@gmail.com>
2019-11-01 16:27:25 +0700

    chore: Remove redundant codes

--- helpers.ts
+++ helpers.ts
@@ -6,7 +6,3 @@ export async function querySelector(selector: string, driver: WebDriver) {
   const el = await driver.wait(until.elementLocated(By.css(selector)), waitUntilTime);
   return await driver.wait(until.elementIsVisible(el), waitUntilTime);
 }
-
-module.exports = {
-  querySelector
-};
